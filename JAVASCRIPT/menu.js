87489dfae12a23c55f839e61fc785e9835805080
aliyassine26 <ali.yassine26@gmail.com>
2020-11-27 01:53:22 +0200

    Major changes (MENU)

--- JAVASCRIPT/menu.js
+++
@@ -1,8 +0,0 @@
-function show_menu(number){
-    
-    var x = document.getElementsByClassName("display");
-for(var i=0;i<=5;i++)
-   if(i != number) 
-   x[i].style.display = "none";
-   else x[i].style.display = "block";
-} 
