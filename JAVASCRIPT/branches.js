622229d1ed9ce8341ea248baa8a6773bfbf5403f
aliyassine26 <ali.yassine26@gmail.com>
2020-11-29 00:54:05 +0200

    new header (responsive)

--- JAVASCRIPT/branches.js
+++ JAVASCRIPT/branches.js
@@ -1,4 +1,12 @@
 // Branches JavaScript
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
 
 function spot() {
   clear();
--- JAVASCRIPT/quiz.js
+++ JAVASCRIPT/quiz.js
@@ -1,5 +1,13 @@
 // Functions
 
+// function myFunction() {
+//    var x = document.getElementById("myTopnav");
+//    if (x.className === "topnav") {
+//      x.className += " responsive";
+//    } else {
+//      x.className = "topnav";
+//    }
+//  }
 
 function buildQuiz() {
     // variable to store the HTML output
